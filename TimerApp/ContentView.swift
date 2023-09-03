6052e8ac4759c3383030b9af3afc7f716fedd358
Berat Yavuz <56202033+brtyvz@users.noreply.github.com>
2023-09-04 00:43:13 +0300

    Update ContentView.swift

--- TimerApp/ContentView.swift
+++ TimerApp/ContentView.swift
@@ -64,8 +64,7 @@ struct ContentView: View {
 
             Button(action: {
                 timerManager.resetTimer()
-                // Zamanlayıcı sıfırlandığında veya değiştirildiğinde
-                // bu işlevi çağırarak zamanı güncelleyin.
+             
                 timerManager.updateTimerBasedOnAppCloseTime()
             }) {
                 Text("Set/Reset")
@@ -80,121 +79,10 @@ struct ContentView: View {
         }
         .onReceive(timerManager.$isTimerCompleted) { completed in
                 if completed {
-                    timerManager.sendNotification() // Zamanlayıcı tamamlandığında bildirimi gönder
+                    timerManager.sendNotification() 
                 }
             }
     }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//import SwiftUI
-//
-//struct ContentView: View {
-//    @EnvironmentObject var timerManager: TimerManager
-//
-//    var body: some View {
-//        VStack {
-//            HStack {
-//                Text("Minutes:")
-//                Picker("Minutes", selection: $timerManager.selectedMinutes) {
-//                    ForEach(0..<60, id: \.self) { minute in
-//                        Text("\(minute)")
-//                    }
-//                }
-//                .pickerStyle(WheelPickerStyle())
-//                .frame(width: 80)
-//
-//                Text("Seconds:")
-//                Picker("Seconds", selection: $timerManager.selectedSeconds) {
-//                    ForEach(0..<60, id: \.self) { second in
-//                        Text("\(second)")
-//                    }
-//                }
-//                .pickerStyle(WheelPickerStyle())
-//                .frame(width: 80)
-//            }
-//            .padding()
-//
-//            RoundedRectangle(cornerRadius: 10)
-//                .frame(width: 200, height: 100)
-//                .foregroundColor(.yellow)
-//                .overlay(
-//                    Text(timerManager.timeString)
-//                        .font(.largeTitle)
-//                )
-//                .padding()
-//
-//            if timerManager.isTimerRunning {
-//                Button(action: {
-//                    timerManager.stopTimer()
-//                }) {
-//                    Text("Pause")
-//                        .font(.title)
-//                        .padding()
-//                        .background(Color.red)
-//                        .foregroundColor(.white)
-//                        .cornerRadius(10)
-//                }
-//            } else {
-//                Button(action: {
-//                    timerManager.startTimer()
-//                }) {
-//                    Text("Play")
-//                        .font(.title)
-//                        .padding()
-//                        .background(Color.green)
-//                        .foregroundColor(.white)
-//                        .cornerRadius(10)
-//                }
-//            }
-//
-//            Button(action: {
-//                timerManager.resetTimer()
-//            }) {
-//                Text("Set/Reset")
-//                    .font(.title)
-//                    .padding()
-//                    .background(Color.gray)
-//                    .foregroundColor(.white)
-//                    .cornerRadius(10)
-//            }
-//            
-//            Spacer()
-//        }
-//    }
-//}
-//
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//            .environmentObject(TimerManager())
-//    }
-//}
